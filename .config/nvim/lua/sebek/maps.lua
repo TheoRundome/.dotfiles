bdb77f26f022ef4612d69714e0ec0ad873fd3dde
Theodor Rundom <teodorrun@protonmail.com>
2024-10-19 14:46:08 +0000

    nvim to lua

---
+++ .config/nvim/lua/sebek/maps.lua
@@ -0,0 +1,28 @@
+vim.g.mapleader = " "
+
+local function map(mode, lhs, rhs)
+	vim.keymap.set(mode, lhs, rhs, { silent = true })
+end
+
+-- Exit insert mode
+map("i", "jj", "<ESC>")
+
+-- NeoTree
+map("n", "<leader>e", "<CMD>Neotree toggle<CR>")
+map("n", "<leader>r", "<CMD>Neotree focus<CR>")
+
+-- New Windows
+map("n", "<leader>o", "<CMD>vsplit<CR>")
+map("n", "<leader>p", "<CMD>split<CR>")
+
+-- Window Navigation
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-j>", "<C-w>j")
+
+-- Resize Windows
+map("n", "<C-Left>", "<C-w><")
+map("n", "<C-Right>", "<C-w>>")
+map("n", "<C-Up>", "<C-w>+")
+map("n", "<C-Down>", "<C-w>-")
--- .zshrc
+++ .zshrc
@@ -49,3 +49,7 @@ export EDITOR=nvim
 
 export PATH=$PATH:/home/sebek/.local/bin
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
